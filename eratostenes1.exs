8c470ab94f085700f16c926dfa7ec1d8c0e5a997
Lucas Rodriguez <lucas.rlanza@udc.es>
2026-02-12 12:34:45 +0100

    Version final Eratostenes

--- eratostenes1.exs
+++ eratostenes1.exs
@@ -1,5 +1,5 @@
 # Implementacion secuencial de primos/1
-defmodule Eratostenes do
+defmodule EratostenesSecuencial do
 
   @spec primos(integer()) :: list(integer())
   def primos(n) when n < 2, do: [] # Si n es menor de 2, no hay números primos
@@ -18,4 +18,68 @@ defmodule Eratostenes do
   defp filtrar(p, [x | xs]) when rem(x, p) == 0, do: filtrar(p, xs) # Si x es múltiplo de p -> descartamos x y filtramos el resto
   defp filtrar(p, [x | xs]), do: [x | filtrar(p, xs)] # Si x no es múltiplo de p -> x es primo, seguimos filtrando el resto
 
+end
+
+# Implementacion concurrente de primos/1
+defmodule EratostenesConcurrente do
+
+  @spec primos(integer()) :: list(integer())
+  def primos(n) when n < 2, do: []
+  def primos(n) when n >= 2 do
+    parent = self()
+
+    case rango(2, n) do
+      [] ->
+        [] # Si el rango esta vacio, no hay primos
+
+      [primero | cola] ->
+        cabeza = spawn(fn -> filtro(primero, nil, parent) end) # Creamos un proceso para el primer primo
+        enviar_numeros(cabeza, cola) # Enviamos el resto de los numeros a proceso cabeza
+        send(cabeza, {:fin, parent}) # Enviamos el mensaje de fin a cabeza
+
+        receive do
+          {:primos, primos} -> primos
+        end
+    end
+
+  end
+
+  defp rango(n, m) when n > m, do: []
+  defp rango(n, m) when n == m, do: [n]
+  defp rango(n, m) when n < m, do: [n | rango(n + 1, m)]
+
+  defp enviar_numeros(_pid, []), do: :ok # Si la lista esta vacia, no hay numeros a enviar
+  defp enviar_numeros(pid, [x | xs]) do
+    send(pid, {:num, x}) # Enviamos el número x al proceso pid
+    enviar_numeros(pid, xs) # Continuamos enviando el resto de la lista
+  end
+
+  defp filtro(primo, siguiente, parent) do
+    receive do
+      {:num, x} ->
+        cond do
+          rem(x, primo) == 0 ->
+            filtro(primo, siguiente, parent) # si x es divisible por primo, descartamos y pasamos el siguiente
+
+          siguiente == nil ->
+            nuevo = spawn(fn -> filtro(x, nil, parent) end)
+            filtro(primo, nuevo, parent) # si no existe un siguiente, creamos un nuevo proceso para el nuevo primo
+
+          true ->
+            send(siguiente, {:num, x}) # si x no es divisible por el primo, se envia al siguiente filtro
+            filtro(primo, siguiente, parent)
+        end
+
+      {:fin, from} ->
+        if siguiente == nil do
+          send(from, {:primos, [primo]}) # si no hay siguiente, enviamos el primo al proceso solicitante
+        else
+          send(siguiente, {:fin, self()}) # si hay siguiente, se envia mensaje de fin al siguiente
+
+          receive do
+            {:primos, primos} -> send(from, {:primos, [primo | primos]}) # recibimos la lista de primos y la enviamos hacia atrás
+          end
+        end
+    end
+  end
 end
